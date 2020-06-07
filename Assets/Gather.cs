da36ebc5464b3d301e2d64c2d737948c85e21b14
blaise <blaise.carnevillier@epitech.eu>
2020-06-07 16:27:35 +0900

    drop gathering + non interactable sliders

---
+++ Assets/Gather.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gather : MonoBehaviour {
+    Transform player;
+    private float distance;
+    public int DistanceMax;
+    Rigidbody2D move;
+    // Use this for initialization
+    void Start () {
+        move = GetComponent<Rigidbody2D> ();
+        player = GameObject.FindGameObjectWithTag ("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        distance = Vector3.Distance (transform.position, player.transform.position);
+        if (distance < DistanceMax) {
+            move.AddForce ((player.transform.position - transform.position) * (16));
+        }
+
+    }
+}
