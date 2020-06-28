b30d8792b9e5b37b2ee3a23b7dc01377b71559f3
Arthjeed <arthjeed@hotmail.fr>
2020-06-28 11:31:21 +0900

    Receive shoot

--- Assets/Prefabs/Destroyer/DestroyerSpawn.cs
+++ Assets/Prefabs/Destroyer/DestroyerSpawn.cs
@@ -12,6 +12,9 @@ public class DestroyerSpawn : MonoBehaviour
     [Range(0,100)]
     public float speed;
     public int chunkNumber = 50;
+
+    [Range(0, 500)]
+    public int distance;
     
     private Vector3 _targetCoreClosestPoint;
     private Transform _childTransform;
@@ -32,6 +35,7 @@ public class DestroyerSpawn : MonoBehaviour
             GameObject tailchunk = Instantiate(tail, transform.position, transform.rotation);
             tailchunk.transform.SetParent(destroyer.transform);
         }
+
         if (Input.GetKey(KeyCode.RightArrow))
 			head.transform.Rotate(0.0f, 2.0f, 0.0f, Space.Self);
 		if (Input.GetKey(KeyCode.LeftArrow))
@@ -40,14 +44,18 @@ public class DestroyerSpawn : MonoBehaviour
             head.transform.position += head.transform.forward * speed * Time.deltaTime;
         }
 
+        /*if (Vector3.Distance(transform.position, target.position) > distance)
+        {
+            head.transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }*/
+
         if (_oneTime == false) {
             for (int chunk = 1; chunk < chunkNumber + 2; chunk++){
                 _childTransform = destroyer.transform.GetChild(chunk).transform;
                 _targetCoreClosestPoint = destroyer.transform.GetChild(chunk - 1).GetChild(0).GetChild(0).GetComponent<CircleCollider2D>().bounds.ClosestPoint(_childTransform.position);
 
                 if ((Vector3.Distance(_targetCoreClosestPoint, _childTransform.position) > 
-                Vector3.Distance(_targetCoreClosestPoint, _childTransform.position + _childTransform.forward * speed * Time.deltaTime))
-                 /*&& (Vector3.Distance(_targetCoreClosestPoint, _childTransform.position + _childTransform.forward * speed * Time.deltaTime) > 0.0f)*/)
+                Vector3.Distance(_targetCoreClosestPoint, _childTransform.position + _childTransform.forward * speed * Time.deltaTime)))
                     _childTransform.position += _childTransform.forward * (speed) * Time.deltaTime;
                 
                 _childTransform.rotation = Quaternion.LookRotation(new Vector3(destroyer.transform.GetChild(chunk - 1).transform.position.x - _childTransform.position.x, 
---
+++ Assets/colisionChunk.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colisionChunk : MonoBehaviour
+{
+    public GameObject crystal;
+    public GameObject lightCrystal;
+    
+    public GameObject target;
+    public GameObject projectile;
+
+   /* void shoot()
+    {
+        Vector2 dir = transform.position - target.transform.positon;
+        Quaternion rotation = Quaternion.LookRotation(new Vector3(dir.x, dir.y, 0));// transform.rotation;// * parent.transform.rotation;
+        GameObject newProj = Instantiate("ProjectileCanon", dir, rotation);
+        //newProj.GetComponent<Projectile>().SetDirection(dir);
+    }*/
+
+
+   void OnTriggerEnter2D(Collider2D col)
+   {
+        if (col.CompareTag("AllyProjectile")) {
+            crystal.SetActive(false);
+            lightCrystal.SetActive(false);
+       }
+   }
+}
