8ec640f9deb9b8a0b10530365381ad2578772b0a
blaise <blaise.carnevillier@epitech.eu>
2020-06-29 23:19:58 +0900

    misc

--- Assets/GameOver.cs
+++ Assets/GameOver.cs
@@ -14,7 +14,7 @@ public class GameOver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.KeypadEnter))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             exit();
         }
